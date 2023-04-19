67cfa0dd6f880d2e6ce80e5a898dc74dfcacee83
Ibis Fortunato <ibisfortunato@gmail.com>
2023-04-19 09:42:21 -0300

    Clase 15 y 16

---
+++ clase_16/app.js
@@ -0,0 +1,28 @@
+let matrix = [
+    [5, 2, 8],
+    [6, 3, 3],
+    [3, 8, 7]
+]
+
+console.log(matrix)
+console.log(matrix[0])
+console.log(matrix[0][0])
+
+
+/* let matriz = []
+let interno1 = [10, 20, 30]
+let interno2 = [30, 40, 50] */
+
+/* matriz.push(interno1, interno2)
+console.log(matriz) */
+
+
+//Matriz con el doble for
+
+for (let i = 0; i < matrix.length; i++) {
+    console.log(`fila numero: ${i}`)
+   for (let j = 0; j < matrix[i].length; j++) {
+    //console.log(matrix[i][j])---> FILA
+    //console.log(matrix[j][i])---> COLUMNA
+   }
+}
