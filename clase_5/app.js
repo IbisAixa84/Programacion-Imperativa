170887a926087e41f8717cc45d58f89d0cc6fca5
Ibis Fortunato <ibisfortunato@gmail.com>
2023-04-26 17:11:19 -0300

    Funciones simples

--- clase_5/app.js
+++ clase_5/app.js
@@ -17,11 +17,11 @@ const prompt = require('prompt-sync')();
 //     return num1 * num2
 // }
 
-// let nombre = 'Ibis'
-// function saludar() {
-//     let nombre = 'Pablo'
-//     return `Hola ${nombre}!!`
-// }
+let nombre = 'Ibis'
+function saludar() {
+    let nombre = 'Pablo'
+    return `Hola ${nombre}!!`
+}
 // console.log('primer log', nombre);
 // console.log('segundo log', saludar());
 
@@ -33,21 +33,19 @@ const prompt = require('prompt-sync')();
 
 
 /* Funciones simples
-En todos los casos en que se reciban parámetros, utilizar el prompt para el ingreso
-
-por consola de los valores-
 1. Crear una función que convierta pulgadas en centímetros.
+Recibe por parámetro pulgadas y retorna su equivalente en centímetros.*/
 
-Recibe por parámetro pulgadas y retorna su equivalente en centímetros.
-2. Crear una función que recibe un string y lo convierte en una URL.
-Ej: “pepito” es devuelto como “http://www.pepito.com”
-3. Crear una función que recibe un string y devuelve la misma frase pero con
-admiración.
-4. Crear una función que calcule la edad de los perros, considerando que 1 año
-para nosotros son 7 de ellos.
-5. Crear una función que calcule el valor de tu hora de trabajo, introduciendo tu
-sueldo mensual como parámetro.
-PD: considerá que tu mes de trabajo tiene 40 horas. */
+function convertirPuldaEnCm(pulgada) {
+    return pulgada * 2.64 + " cm"
+    
+}
+
+console.log(convertirPuldaEnCm(20));
+
+
+/*2. Crear una función que recibe un string y lo convierte en una URL.
+Ej: “pepito” es devuelto como “http://www.pepito.com” */
 
 let otroNombre = "function"
 
@@ -55,7 +53,36 @@ function convertirString(palabra) {
     return `http://www.${palabra}.com`
 }
 
-console.log(convertirString(pepe));
+//console.log(convertirString(pepe))
 
 
+/*3. Crear una función que recibe un string y devuelve la misma frase pero con
+admiración.*/
+
+function imprimirFrase(strings) {
+    return `¡${strings}!`
+}
+
+console.log(imprimirFrase("Hola mi ciela"))
+
+
+/*4. Crear una función que calcule la edad de los perros, considerando que 1 año
+para nosotros son 7 de ellos.*/
+
+function calcularEdadCanina(edad) {
+    return edad * 7
+}
+
+console.log(calcularEdadCanina(2));
+
+
+/*5. Crear una función que calcule el valor de tu hora de trabajo, introduciendo tu
+sueldo mensual como parámetro.
+PD: considerá que tu mes de trabajo tiene 40 horas. */
+
+function calcularHoraLaboral(sueldo) {
+    return sueldo / 40
+}
+
+console.log(calcularHoraLaboral(1000))
 
