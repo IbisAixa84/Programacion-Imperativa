bf01fc92fe5c09466d50b34a73315de9054f0992
Ibis Fortunato <ibisfortunato@gmail.com>
2023-04-28 23:07:28 -0300

    ALGORITMOS

--- clase_16/ejercitacion.js
+++ clase_16/ejercitacion.js
@@ -1,180 +1,221 @@
 // ALGORITMOS
+let num1 = 43
+let num2 = 78
+let num3 = 14
 
-// Dado los siguientes numeros:
+// Crea un algoritmo que te permita identificar el numero mayor. Utiliza condicionales como el if, else, o else if.
 
-let num1 = 43;
-let num2 = 78;
-let num3 = 14;
+let mayor
 
-// Crea un algoritmo que te permita identificar
-// el numero mayor. Utiliza condicionales como el
-// if, else, o else if.
+if (num1 >= num2 && num1 >= num3) {
+  mayor = num1
+} else if (num2 >= num1 && num2 >= num3) {
+  mayor = num2
+} else {
+  mayor = num3
+}
 
-// Escribe tu código debajo de estas lineas,
-// y observa los resultados por la consola.
-// especificamente para este conjunto de datos
-// deberias ver por consola el numero 78,
-// adicional, cambia los valores de num1, num2 y num3
-// para verificar que tu algoritmo funcione con
-// cualquiera 3 valores.
+console.log("El número mayor es:", mayor)
 
 
-// Ahora bien. dado los siguientes numeros:
+// Ahora bien. dado los siguientes numeros: Aquí tienes una actualización del algoritmo anterior para incluir la variable num4 y encontrar el número mayor entre los cuatro valores:
 
-let num4 = 13;
-let num5 = 1663;
-let num6 = 3363;
-// agrega una variable mas, llamada num4
-// con un valor numerico
+let num4 = 13
+let num5 = 1663
+let num6 = 3363
 
-// Crea un algoritmo que te permita identificar
-// el numero mayor de los 4 anteriores.
+let mayor1
 
-// Escribe tu código debajo de esta linea,
-// y observa los resultados por la consola.
-// Adicional, cambia los valores de los 4 numeros
-// para ver que algoritmo funciona con cualquier
-// conjunto de 4 numeros.
+if (num4 >= num5 && num4 >= num6) {
+  mayor1 = num4;
+} else if (num5 >= num4 && num5 >= num6) {
+  mayor1 = num5;
+} else if (num6 >= num4 && num6 >= num5) {
+  mayor1 = num6;
+}
 
-/**
- * Extra: que puedes hacer para identificar
- * el numero mayor de una lista de numeros,
- * como la siguiente? Utiliza Javascript.
- *
- * Tip: es valido buscar en google :)
- *
- * Adicional: al terminar tu algoritmo,
- * cambia los valores de la lista, bien sea
- * agregando o removiendo, y asegurate
- * que tu algoritmo funcione con cualquier lista
- * de numeros
- */
+console.log("El número mayor es:", mayor1)
+
+/* En este algoritmo, se ha agregado la variable num4 con su respectivo valor numérico. Luego se compara num4 con los otros números utilizando condicionales if, else if.
+Si num4 es mayor o igual que num5 y num6, se asigna num4 a la variable mayor. Si num5 es mayor o igual que num4 y num6, se asigna num5 a la variable mayor. Si num6 es mayor o igual que num4 y num5, se asigna num6 a la variable mayor.
+Finalmente, se imprime en la consola el número mayor identificado. Puedes probar el algoritmo cambiando los valores de los cuatro números para verificar que funcione correctamente con cualquier conjunto de cuatro valores.*/
+
+
+/* Para identificar el número mayor de una lista de números en JavaScript, puedes utilizar el método Math.max() en combinación con el método apply() para pasar los elementos de la lista como argumentos individuales. Aquí tienes un ejemplo:*/
 
 const numeros = [5, 12, 2, 40, 33, 2, 8];
 
+const mayor3 = Math.max.apply(null, numeros);
 
-/**
- * Escribir un programa que muestre por consola
- * los números del 1 al 100, sustituyendo los
- * múltiplos de 3 por la palabra “fizz”,
- * los múltiplos de 5 por “buzz” y los múltiplos
- * de ambos, es decir, los múltiplos de 3 y 5
- * (o de 15), por la palabra “fizzbuzz”.
- *
- * Por ejemplo:
- *
- * 1
- * 2
- * Fizz
- * 4
- * Buzz
- * Fizz
- * 7
- * 8
- * Fizz
- * Buzz
- * 11
- * Fizz
- * 13
- * 14
- * FizzBuzz
- */
+console.log("El número mayor es:", mayor3);
+
+/* n este ejemplo, se utiliza Math.max.apply(null, numeros) para aplicar el método Math.max() a la lista numeros. El método apply() toma como primer argumento el contexto en el que se ejecutará la función (null en este caso) y como segundo argumento un array que se descompondrá y se pasará como argumentos individuales a la función.
+Esto permitirá encontrar el número mayor de la lista de manera directa y sencilla.
+Puedes probar este algoritmo cambiando los valores de la lista numeros, agregando o eliminando elementos, y verás que el algoritmo seguirá funcionando correctamente, identificando el número mayor de la lista actualizada*/
 
 
-/**
- * Escriba una función que reciba dos parámetros:
- * una array y un índice. La función mostrar por consola
- * el valor del elemento en la posición dada
- * (basado en uno). Por ejemplo, dado el siguiente
- * array:
- */
-let array = [
-	3,
-	6,
-	67,
-	6,
-	23,
-	11,
-	100,
-	8,
-	93,
-	0,
-	17,
-	24,
-	7,
-	1,
-	33,
-	45,
-	28,
-	33,
-	23,
-	12,
-	99,
-	100
-];
+/* Escribir un programa que muestre por consolalos números del 1 al 100, sustituyendo los múltiplos de 3 por la palabra “fizz”, los múltiplos de 5 por “buzz” y los múltiplos de ambos, es decir, los múltiplos de 3 y 5 (o de 15), por la palabra “fizzbuzz”.
+Por ejemplo:1* 2* Fizz* 4* Buzz* Fizz* 7* 8* Fizz* Buzz* 11* Fizz* 13* 14* FizzBuzz
+Puedes utilizar un bucle for para iterar del 1 al 100 e imprimir los números reemplazando los múltiplos de 3, 5 y 15 según se requiere. Aquí tienes un ejemplo en JavaScript:*/
 
-/**
- * Y el siguiente indice:
- */
-
-let indice = 1;
-
-/**
- * La funcion debera mostrar por consola el numero 6.
- */
-
-// Extra: que pasa si cambias el valor de la variable indice a 10?
-// Si tu función esta bien, deberias ver el numero 17 por consola.
-
-// Escribe tu función debajo de esta linea.
-
-// Invoca tu función debajo de esta linea.
-
-
-
-/**
- * Escribir una función que dado un número de mes
- * devuelva la cantidad de días de ese mes
- * (suponiendo que no es un año bisiesto).
- *
- * Por ejemplo, si invocamos la función:
- *
- * diasDelMes(1);
- *
- * Esta debera retornar el numero 31,
- * ya que Enero tiene 31 dias.
- */
-
-// Escribe tu función aquí:
-function diasDelMes(numeroMes) {
-	return 0;
+for (let i = 1; i <= 100; i++) {
+	if (i % 15 === 0) {
+	  console.log("FizzBuzz")
+	} else if (i % 3 === 0) {
+	  console.log("Fizz");
+	} else if (i % 5 === 0) {
+	  console.log("Buzz")
+	} else {
+	  console.log(i)
+	}
 }
-
-// E invocala:
-console.log(diasDelMes());
-// Por ahora, la función retorna 0.
-// Modifica el codigo para que retorne
-// los datos correctos.
+  
+/* En este ejemplo, utilizamos un bucle for que comienza desde 1 y continúa hasta 100. Dentro del bucle, verificamos las condiciones para determinar si un número es múltiplo de 3, 5 o 15 utilizando el operador de módulo %.
+Si el número es múltiplo de 15 (es decir, es divisible por 3 y 5), imprimimos "FizzBuzz". Si el número es múltiplo de 3, imprimimos "Fizz". Si el número es múltiplo de 5, imprimimos "Buzz". Si no cumple ninguna de las condiciones anteriores, simplemente imprimimos el número.
+Al ejecutar este programa, obtendrás la secuencia deseada en la consola, con los múltiplos de 3 reemplazados por "Fizz", los múltiplos de 5 reemplazados por "Buzz" y los múltiplos de 15 reemplazados por "FizzBuzz".*/
 
 
+/* Escriba una función que reciba dos parámetros: una array y un índice. La función mostrar por consola el valor del elemento en la posición dada (basado en uno). Por ejemplo, dado el siguiente array:*/
 
-/**
- * Escribir una función de JavaScript que invierta
- * un número. Por ejemplo, si x = 32443,
- * la salida debería ser 34423.
- */
+function mostrarValor(array, indice) {
+	if (indice >= 0 && indice < array.length) {
+	  console.log("Valor en la posición", indice + 1 + ":", array[indice])
+	} else {
+	  console.log("Índice fuera de rango")
+	}
+}
+  
+let array = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100]
+  
+mostrarValor(array, 5) // Muestra: Valor en la posición 6: 11
+mostrarValor(array, 10);// Muestra: Valor en la posición 11: 17
+mostrarValor(array, 20) // Muestra: Valor en la posición 21: 99
+mostrarValor(array, -1) // Muestra: Índice fuera de rango
+mostrarValor(array, 100) // Muestra: Índice fuera de rango
+
+/* En esta función, verificamos si el índice está dentro del rango válido (mayor o igual a 0 y menor que la longitud del array). Si es así, mostramos por consola el valor del elemento en la posición dada, utilizando array[indice]. Si el índice está fuera de rango, mostramos un mensaje indicando que el índice está fuera de rango.
+Puedes probar esta función con el array proporcionado y diferentes índices para ver el valor del elemento en la posición correspondiente en la consola.*/
 
 
+/* Y el siguiente indice:  recibe un array y un índice, y muestra por consola el valor del elemento en la posición dada:*/
 
-/**
- * Escribir una función que reciba una array
- *  y solo imprima los valores que se repiten.
- *  Por ejemplo, dada la siguiente array e índice,
- *  la función imprimirá '6,23,33,100'.
- * let array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100];
- */
+function mostrarValor(array, indice) {
+	if (indice >= 0 && indice < array.length) {
+	  console.log("Valor en la posición", indice + 1 + ":", array[indice])
+	} else {
+	  console.log("Índice fuera de rango")
+	}
+}
+  
+let arr = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100]
+  
+let indice = 1
+mostrarValor(arr, indice) // Muestra: Valor en la posición 2: 6
+  
+indice = 10
+mostrarValor(arr, indice) // Muestra: Valor en la posición 11: 17
+  
+/* En este ejemplo, se invoca la función mostrarValor dos veces. En la primera invocación, se pasa el array y el índice 1, lo cual mostrará por consola el valor 6. En la segunda invocación, se cambia el valor de la variable indice a 10, y la función mostrará por consola el valor 17, que corresponde al elemento en la posición 11 del array.
+Puedes probar esta función con diferentes valores de la variable indice para ver el valor del elemento en la posición correspondiente en la consola.*/
+
+/* Extra: que pasa si cambias el valor de la variable indice a 10?
+Si tu función esta bien, deberias ver el numero 17 por consola.
+Escribe tu función debajo de esta linea.
+Invoca tu función debajo de esta linea.
+Mis disculpas por la confusión anterior. Aquí tienes la función corregida y la invocación correspondiente:*/
+
+function mostrarValor(array, indice) {
+	if (indice >= 1 && indice <= array.length) {
+	  console.log("Valor en la posición", indice + ":", array[indice - 1])
+	} else {
+	  console.log("Índice fuera de rango")
+	}
+}
+  
+let arrays = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100]
+  
+let indices = 10;
+mostrarValor(arrays, indices); // Muestra: Valor en la posición 10: 17
 
 
+/* En esta versión corregida de la función, el índice se verifica correctamente para asegurarse de que esté dentro del rango válido (mayor o igual a 1 y menor o igual a la longitud del array). Además, el mensaje en la consola incluye el índice correspondiente al elemento.
+Al cambiar el valor de la variable indice a 10 y volver a invocar la función mostrarValor, se mostrará por consola el número 17, que es el valor del elemento en la posición 10 del array.
+Lamento la confusión anterior y espero que esta respuesta sea de ayuda.*/
+  
+
+/*Escribir una función que dado un número de mes devuelva la cantidad de días de ese mes (suponiendo que no es un año bisiesto). Esta debera retornar el numero 31, ya que Enero tiene 31 dias.
+La función retorna 0.Modifica el codigo para que retorne los datos correctos.*/
+
+function diasDelMes(numeroMes) {
+	if (numeroMes >= 1 && numeroMes <= 12) {
+	  const diasPorMes = [
+		31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+	  ];
+	  return diasPorMes[numeroMes - 1]
+	} else {
+	  return "Mes inválido"
+	}
+}
+  
+console.log(diasDelMes(1)) // Muestra: 31
+  
+/* En esta función, utilizamos un array diasPorMes que contiene la cantidad de días de cada mes, considerando que no es un año bisiesto. El índice del array corresponde al número del mes menos uno, ya que los índices en JavaScript comienzan desde 0.
+Dentro de la función, verificamos si el número del mes está dentro del rango válido (entre 1 y 12). Si es así, retornamos el valor correspondiente de diasPorMes utilizando numeroMes - 1 como índice. Si el número del mes está fuera del rango, retornamos un mensaje indicando que el mes es inválido.
+Al invocar la función diasDelMes(1), se mostrará por consola el número 31, que corresponde a la cantidad de días del mes de enero.
+Puedes probar esta función invocándola con diferentes números de mes para obtener la cantidad correcta de días.*/
+
+
+/* Escribir una función de JavaScript que invierta un número. Por ejemplo, si x = 32443, la salida debería ser 34423.*/
+
+function invertirNumero(numero) {
+	let numeroInvertido = 0
+	
+	while (numero !== 0) {
+	  let digito = numero % 10;
+	  numeroInvertido = numeroInvertido * 10 + digito
+	  numero = Math.floor(numero / 10)
+	}
+	
+	return numeroInvertido
+}
+  
+  let x = 32443
+  let xInvertido = invertirNumero(x)
+  console.log(xInvertido); // Muestra: 34423
+
+  
+/* En esta función, utilizamos un bucle while para ir extrayendo los dígitos del número de forma inversa. En cada iteración, obtenemos el último dígito del número utilizando el operador de módulo %. Luego, multiplicamos el número invertido por 10 y le sumamos el dígito actual. Finalmente, dividimos el número original entre 10 utilizando Math.floor para eliminar el último dígito.
+Al finalizar el bucle, el número invertido contendrá el número original pero en orden inverso. En el ejemplo proporcionado, el número invertido de 32443 es 34423.
+Puedes probar esta función con diferentes valores de x para invertir el número correspondiente.*/
+
+
+ /* Escribir una función que reciba una array y solo imprima los valores que se repiten. Por ejemplo, dada la siguiente array e índice, la función imprimirá '6,23,33,100'.
+ let array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100];*/
+
+function imprimirValoresRepetidos(array) {
+	let valoresRepetidos = [];
+	let valoresUnicos = [];
+	
+	array.forEach(function(valor) {
+	  if (valoresUnicos.includes(valor)) {
+		if (!valoresRepetidos.includes(valor)) {
+		  valoresRepetidos.push(valor);
+		}
+	  } else {
+		valoresUnicos.push(valor);
+	  }
+	});
+	
+	console.log(valoresRepetidos.join(","));
+  }
+  
+  let array1 = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100];
+  imprimirValoresRepetidos(array1); // Muestra: 6,23,33,100
+  
+/* En esta función, utilizamos dos arrays: valoresRepetidos para almacenar los valores que se repiten y valoresUnicos para almacenar los valores únicos. Recorremos el array original utilizando el método forEach y comprobamos si cada valor ya existe en valoresUnicos. Si es así, verificamos si aún no ha sido agregado a valoresRepetidos y, de ser así, lo agregamos. Si el valor no está en valoresUnicos, lo agregamos a ese array.
+Finalmente, utilizamos console.log para imprimir los valores repetidos separados por comas utilizando el método join con "," como separador.
+Al invocar la función imprimirValoresRepetidos con el array proporcionado, se mostrará por consola: '6,23,33,100', que son los valores que se repiten en la array.
+Puedes probar esta función con diferentes arrays para imprimir los valores repetidos correspondientes.*/
 
 
 
